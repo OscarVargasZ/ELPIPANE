49f792bc556441871a96db942dede59838b493ac
OscarVargas97 <43867089+OscarVargas97@users.noreply.github.com>
2019-06-03 22:13:44 -0400

    oscar
    agregando routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,13 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BarraComponent } from './barra/barra.component';
-import { ColumnasComponent } from './columnas/columnas.component';
-import { LoginComponent } from './login/login.component';
-import { CarritoComponent } from './carrito/carrito.component';
-import { ArticuloComponent } from './articulo/articulo.component';
-import { CarrucelComponent } from './carrucel/carrucel.component';
-import { FooterComponent } from './footer/footer.component';
+import { BarraComponent } from './componentes/barra/barra.component';
+import { ColumnasComponent } from './componentes/columnas/columnas.component';
+import { LoginComponent } from './componentes/login/login.component';
+import { CarritoComponent } from './componentes/carrito/carrito.component';
+import { ArticuloComponent } from './componentes/articulo/articulo.component';
+import { CarrucelComponent } from './componentes/carrucel/carrucel.component';
+import { FooterComponent } from './componentes/footer/footer.component';
 
 @NgModule({
   declarations: [
